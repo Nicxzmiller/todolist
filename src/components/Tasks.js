f9b99a81dac4bf377e8b44104dbd4b47cd35026d
Nicholas <nicxzmiller2@gmail.com>
2020-07-18 15:52:27 +0200

    styling nav

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -14,7 +14,7 @@ export const Tasks = () =>{
             <h2 data-testId="project-name">{projectName}</h2>
 
             <ul className="task__list">
-                {tasks.map(task => (
+                {tasks && tasks.map(task => (
                     <li key={`${task.id}`}>
                         <Checkbox id={task.id}/>
                         <span>{task.task}</span>
