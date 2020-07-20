2e70a2a7650ce12fc8d1db6bb767d70aee93207e
Nicholas <nicxzmiller2@gmail.com>
2020-07-20 19:42:23 +0200

    changes

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Checkbox} from "./Checkbox";
 import {useTasks} from "../hooks";
+import { collatedTasks } from "../constants";
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from "../context";
 
 export const Tasks = () =>{
-    const { tasks } = useTasks("1");
-
-    console.log(tasks);
+    const { selectedProject } = useSelectedProjectValue();
+    const { projects } = useProjectsValue();
+    const { tasks } = useTasks(selectedProject);
 
     let projectName = '';
+
+    if(projects && selectedProject && !collatedTasksExist(selectedProject)){
+        projectName = getTitle(projects, selectedProject).name;
+        console.log('Project Name 1: ', projectName);
+    }
+
+    if(collatedTasksExist(selectedProject) && selectedProject){
+        projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+        console.log('Project Name 2: ', projectName);
+    }
+
+    useEffect(() => {
+        document.title = `${projectName}: TodoList`;
+    });
 
     return(
         <div className="tasks" data-testid="tasks">
